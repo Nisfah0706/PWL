2e6a4281f3b537780a7eb72b67effd9cd878a111
Nisfah0706 <111202415915@mhs.dinus.ac.id>
2026-04-08 23:44:10 +0700

    Update routing for products feature

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,6 +9,6 @@ $routes->get('/', 'Home::index');
 $routes->get('/produk', 'ProdukController::index');
 $routes->get('/keranjang', 'TransaksiController::index');
 
-$routes->post('products', 'Product::feature');
-$routes->put('products/1', 'Product::feature');
-$routes->delete('products/1', 'Product::feature');
+$routes->post('produk', 'ProdukController::feature');
+$routes->put('produk/1', 'ProdukController::feature');
+$routes->delete('produk/1', 'ProdukController::feature');

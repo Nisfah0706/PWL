8e75499a32cc7bb6dd9fa668dbabae3d1d516455
Nisfah0706 <111202415915@mhs.dinus.ac.id>
2026-04-21 07:34:45 +0700

    update login logout dan layout ci4

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,9 +6,11 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login');
+$routes->get('logout', 'AuthController::logout');
+
 $routes->get('/produk', 'ProdukController::index');
 $routes->get('/keranjang', 'TransaksiController::index');
 
-$routes->post('produk', 'ProdukController::feature');
-$routes->put('produk/1', 'ProdukController::feature');
-$routes->delete('produk/1', 'ProdukController::feature');
